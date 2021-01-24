8675b8618f50a30682fb206242a50ee188a82225
Sravya Balasa <sravya.balasa@gmail.com>
2021-01-24 12:53:24 -0800

    prod changes

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const mongoDB = require('./db');
 
 // constants
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const HOST = '0.0.0.0';
 
 // initialize the db connection + app
--- frontend/src/conf.js
+++ frontend/src/conf.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
-const url = 'http://localhost:2020';
+// const url = 'http://localhost:2020';
+const url = 'https://mode-humana.herokuapp.com'
 
 const api = axios.create({
 	baseURL: url
 })
+
+console.log(url);
 
 export default api;
